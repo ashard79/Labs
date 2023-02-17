af6d67413975c9f68b79aeb1a45414392dfd7737
ashard79 <ashard79@yahoo.com>
2023-02-17 05:34:11 +0000

    added file hello world

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Osaremeh Abel
+# Date of latest revision: 02/16/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo hi im oz 1
+
+pwd
+whoami
+# End
