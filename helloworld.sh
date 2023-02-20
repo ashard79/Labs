0c4cd766dce1ffa4cba34d124e3cda1007651247
ashard79 <ashard79@yahoo.com>
2023-02-20 00:49:06 +0000

    hello there

--- helloworld.sh
+++ helloworld.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 02 Ops Challenge Solution
+# Script: Ops 201 Class 03 Ops Challenge Solution
 # Author: Osaremeh Abel
-# Date of latest revision: 02/16/23
-# Purpose: Print a string to the terminal
+# Date of latest revision: 02/19/23
+# Purpose: use at least one funtion and one variable
 
 
 # Main
-echo hi im oz 1
-echo i am working perfectly
-pwd
-whoami
+
+echo "the stores at the mall are really really tall"
+echo "stores at the plaza are really really small"
+echo 5432
+
+print_somthing() { osaremehabel@osaremehs-MacBook-Pro ~ % last
+osaremehabel  ttys000                   Sat Feb 18 17:09   still logged in
+osaremehabel  ttys000                   Sat Feb 18 16:11 - 16:11  (00:00)
+osaremehabel  ttys000                   Sat Feb 18 10:54 - 10:54  (00:00)
+osaremehabel  console                   Sat Feb 18 10:54   still logged in
+reboot    ~                         Sat Feb 18 10:54 
+osaremehabel  ttys000                   Wed Feb 15 00:34 - 00:34  (00:00)
+osaremehabel  ttys000                   Wed Feb 15 00:33 - 00:33  (00:00)
+osaremehabel  ttys000                   Tue Feb 14 23:51 - 23:51  (00:00)
+osaremehabel  ttys000                   Tue Feb 14 23:44 - 23:44  (00:00)
+osaremehabel  console                   Tue Feb 14 20:27 - 00:59 (2+04:31)
+reboot    ~                         Tue Feb 14 20:27 
+osaremehabel  console                   Sun Feb 12 08:46 - crash (2+11:40
+
+}
+
+
 # End
