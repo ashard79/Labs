ee1393998ce0ec3fd882c7de8ab0ac3ac2dac754
ashard79 <ashard79@yahoo.com>
2023-02-21 04:42:56 +0000

    history

--- helloworld.sh
+++ helloworld.sh
@@ -8,33 +8,13 @@
 
 # Main
 
-echo "the stores at the mall are really really tall"
-echo "stores at the plaza are really really small"
-echo 5432
-
-varname=lets go
-varname2=please stop
-
-echo $varname
-
-echo $varname2
-
-print_somthing() { osaremehabel@osaremehs-MacBook-Pro ~ % last
-osaremehabel  ttys000                   Sat Feb 18 17:09   still logged in
-osaremehabel  ttys000                   Sat Feb 18 16:11 - 16:11  (00:00)
-osaremehabel  ttys000                   Sat Feb 18 10:54 - 10:54  (00:00)
-osaremehabel  console                   Sat Feb 18 10:54   still logged in
-reboot    ~                         Sat Feb 18 10:54 
-osaremehabel  ttys000                   Wed Feb 15 00:34 - 00:34  (00:00)
-osaremehabel  ttys000                   Wed Feb 15 00:33 - 00:33  (00:00)
-osaremehabel  ttys000                   Tue Feb 14 23:51 - 23:51  (00:00)
-osaremehabel  ttys000                   Tue Feb 14 23:44 - 23:44  (00:00)
-osaremehabel  console                   Tue Feb 14 20:27 - 00:59 (2+04:31)
-reboot    ~                         Tue Feb 14 20:27 
-osaremehabel  console                   Sun Feb 12 08:46 - crash (2+11:40
-
+echo ""
+echo ""
+apple="last"
+my_history (){
+$apple
 }
 
-
+my_history
 
 # End
