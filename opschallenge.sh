f9646c7186a70febc4cd16a091ed9eae4079dd90
ashard79 <ashard79@yahoo.com>
2023-02-21 05:18:33 +0000

    practice

---
+++ opschallenge.sh
@@ -0,0 +1,18 @@
+# #!/bin/bash
+
+# Script: ops 201 class 03 ops Challenge
+# Author: Osaremeh Abel
+# Date of latest revision: 02/19/23
+# Purpose: Use at least one funtion and one variable
+
+
+# Main 
+pear=who am i
+history{} { 
+    $pear                
+
+}
+
+history
+
+# End
