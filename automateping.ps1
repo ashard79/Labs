d7685c62bb3df53ce526d566ee1a990ceaeed0cc
ashard79 <ashard79@yahoo.com>
2023-04-09 00:34:46 +0000

    ping auto

---
+++ automateping.ps1
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+$hostname = "espn.com"
+$pingResult = Test-Connection $hostname -Count 1 -ErrorAction SilentlyCountinue
+if ($pingResult -ne $null) {
+    Write-Output "$hostname is online."
+}
+else {
+    Write-Output "$hostname is offline."
+}
