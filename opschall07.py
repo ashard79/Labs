02a193e29753fd68b61219820bfa786393386f8d
ashard79 <ashard79@yahoo.com>
2023-05-14 01:45:40 +0000

    pushed but not started

---
+++ opschall07.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Script: ops 301 class 07 ops Challenge
+# Author: Osaremeh Abel
+# Date of latest revision: 05/13/23
+# Purpose: Create a Python script that generates all directories sub-directories 
+# and files for a user-provided directory path.
+# Script must ask the user for a file path and read a user input string into a variable.
+# Script must use the os.walk() function from the os library.
+# Script must enclose the os.walk() function within a python function that hands it the user input file path.
+
+# Main
+
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+### Read user input here into a variable
+
+# Declaration of functions
+
+### Declare a function here
+
+for (root, dirs, files) in os.walk("testdir"):
+    ### Add a print command here to print ==root==
+    print(root)
+    ### Add a print command here to print ==dirs==
+    print(dirs)
+    ### Add a print command here to print ==files==
+    print(files)
+
+# Main
+
+### Pass the variable into the function here
+
+# End
+
+
+
