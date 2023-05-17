9b4e5f29f43b54ccbb1e9866bd656c5f4b3ee9e5
ashard79 <ashard79@yahoo.com>
2023-05-17 16:26:03 +0000

    ops challange 07

--- opschall07.py
+++ opschall07.py
@@ -11,31 +11,21 @@
 
 # Main
 
-#!/usr/bin/env python3
-
-# Import libraries
-
 import os
 
-# Declaration of variables
+### Define function
 
-### Read user input here into a variable
 
-# Declaration of functions
-
-### Declare a function here
-
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
+def file_show(paths):
+    for (root, dirs, files) in os.walk(paths):
+        print(root)
+        print(dirs)
+        print(files)
 
 # Main
 
-### Pass the variable into the function here
+paths = input("Please choose a directory for infomation!")
+file_show(paths)
 
 # End
 
