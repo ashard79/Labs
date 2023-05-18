b51d94ed20ac78c43dcc44c1ebc36ab3ad3daeef
ashard79 <ashard79@yahoo.com>
2023-05-18 04:16:29 +0000

    ops chall pused but not done

---
+++ opschall08.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Script: ops 301 class 08 ops Challenge
+# Author: Osaremeh Abel
+# Date of latest revision: 05/18/23
+# Purpose: Crrate a python script that
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+# Create one tuple. Create one set. Create one dictionary.
+
+
+
+# Main
+
+append()
+clear()
+copy()
+count()
+extend()
+index()
+insert()
+pop()
+remove()
+reverse()
+sort()
+
+
+# End
+
+
+
