12e7e81ea4c665a57afd236df21998f71493f4cb
ashard79 <ashard79@yahoo.com>
2023-05-19 21:07:01 +0000

    ops challange08 done

--- opschall08.py
+++ opschall08.py
@@ -9,22 +9,60 @@
 # Print the sixth through tenth element of the list.
 # Change the value of the seventh element to “onion”.
 # Create one tuple. Create one set. Create one dictionary.
+# append()
+# clear()
+# copy()
+# count()
+# extend()
+# index()
+# insert()
+# pop()
+# remove()
+# reverse()
+# sort()
 
 
 
 # Main
+import time
 
-append()
-clear()
-copy()
-count()
-extend()
-index()
-insert()
-pop()
-remove()
-reverse()
-sort()
+# Created tuple
+my_fruits = ["apples", "pears", "grapes", "watermelon", "strawberrys", "oranges", "bannanas", "cherrys",
+            "plums","pineapples"]
+# Created Set
+my_teams = {"jets", "yankees", "knicks", "cubs", "nets", "bills", "mets", "heat", "magic", "rams"}
+
+# created dictionary
+my_life = {
+
+    'Name': 'Osaremeh',
+    'sports': 'Football',
+    'Hobbies': 'Music',
+    'movie': 'Malcolm x',
+
+}
+
+print("fruit list.")
+# Printed list
+print(my_fruits)
+# Printed fourth element of the list
+print(my_fruits[3], "!")
+# Printed the sixth through tenth element of the list.
+print(my_fruits[5:10])
+# Change the value of the seventh element to “onion”.
+my_fruits.remove("bannanas")
+my_fruits.insert(6, "onions")
+time.sleep(2)
+print(my_fruits)
+# Append list
+my_fruits.append("Grapefruits")
+print(my_fruits)
+# reverse list
+my_fruits.reverse()
+print(my_fruits)
+# count within the list
+my_fruits.count(2)
+
 
 
 # End
