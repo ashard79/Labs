e49a432f4d67a7e913bacfcc5738a79065645f84
ashard79 <ashard79@yahoo.com>
2023-06-01 04:06:29 +0000

    ops chall12 not finnished

---
+++ opschall12.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Script: ops 301 class 12 Ops Challenge
+# Author: Osaremeh Abel
+# Date of latest revision: 05/31/23
+# Purpose: Prompt the user to type a string input as the variable for your destination URL.
+# Prompt the user to select a HTTP Method of the following options:
+# GET, POST, PUT DELETE,H EAD, PATCH, OPTIONS
+# Print to the screen the entire request your script is about to send. Ask the user to confirm before proceeding.
+# Using the Requests library, perform a GET request against your lab web server.
+# For the given header, translate the codes into plain terms that print to the screen; 
+# for example, a 404 error should print Site not found to the terminal instead of 404.
+# For the given URL, print response header information to the screen.
+
+
+
+
+# Main
+
+# Prompting user to enter destination URL
+destination_url = input("Enter the destination URL: ")
+
+# Use the entered URL 
+print("Destination URL:", destination_url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
